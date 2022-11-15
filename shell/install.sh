7d16fa31aae6c4e8dfdf8b5cb9fb48868d63f773
Orzo Cogorzo <orzocogorzo@hotmail.com>
2022-11-15 18:50:44 +0100

    fix: Fix iptables port opening

--- shell/install.sh
+++ shell/install.sh
@@ -1,11 +1,28 @@
 #! /usr/bin/env bash
 
+cd $(dirname $0) && cd ..
+if ! [ -f .env ]; then
+    echo "You should place a .env file in the root directory of the repository"
+    exit 1
+fi
+
+source .env
+
+if [ -z "$DYNIPT_HOST_IP" ]; then
+    echo "You have to set your host ip in the .env file"
+    exit 1
+fi
+
+sudo apt update
 sudo apt install -y sudo iptables python3 python3-venv git nginx
 sudo useradd -M -s /usr/sbin/nologin dynipt
 
-read -p "Set dynipt user password: " dynipt_pwd
-(echo $dynipt_pwd, echo $dynipt_pwd) | sudo passwd dynipt
-sed -i "s/DYNIPT_PWD=.*/DYNIPT_PWD=$dynipt_pwd/" .env
+if [ -z "$DYNIPT_PWD" ]; then
+    read -p "Set dynipt user password: " DYNIPT_PWD
+    (echo $DYNIPT_PWD, echo $DYNIPT_PWD) | sudo passwd dynipt
+    sed -i "s/DYNIPT_PWD=.*/DYNIPT_PWD=$DYNIPT_PWD/" .env
+fi
+
 sudo usemod -aG sudo dynipt
 sudo git clone https://github.com/orzocogorzo/dyniptables.git /opt/dyniptables
 sudo chown -R dynipt: /opt/dyniptables
@@ -14,7 +31,7 @@ cd /opt/dyniptables
 sudo -u dynipt python3 -m venv .venv
 sudo -u dynipt .venv/bin/python -m pip install -r requirements.txt
 
-sudo iptables -t filter -A FORWARD -p tcp -s $(ip a) --dport 8008 -j ACCEPT
+sudo iptables -t filter -A FORWARD -p tcp -s $DYNIPT_HOST_IP --dport 8008 -j ACCEPT
 
 sudo cp snippets/nginx.conf /etc/nginx/conf.d/dyniptables.conf
 sudo cp snippets/systemd.service /etc/systemd/system/dyniptables.service
