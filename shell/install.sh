3694a74fc9bc68b73d8486cc020b88c9df778c9a
Orzo Cogorzo <orzocogorzo@hotmail.com>
2022-11-15 23:43:22 +0100

    fix: Insert insted of append port 8008 rule

--- shell/install.sh
+++ shell/install.sh
@@ -15,7 +15,7 @@ sudo git clone https://github.com/orzocogorzo/dyniptables.git /opt/dyniptables
 sudo chown -R dynipt: /opt/dyniptables
 
 # Setup config
-HOST_PUBLIC_IP=$(curl https://ip.yunohost.org)
+HOST_PUBLIC_IP=$(curl ip.yunohost.org)
 sudo -u dynipt sed -i "s/DYNIPT_HOST_IP=.*/DYNIPT_HOST_IP=$HOST_PUBLIC_IP/" /opt/dyniptables/.env
 sudo -u dynipt sed -i "s/DYNIPT_PWD=.*/DYNIPT_PWD=$DYNIPT_PWD/" /opt/dyniptables/.env
 
@@ -25,7 +25,7 @@ sudo -u dynipt python3 -m venv .venv
 sudo -u dynipt .venv/bin/python -m pip install -r requirements.txt
 
 # Open dyniptables port
-sudo iptables -t filter -A FORWARD -p tcp -s $HOST_PUBLIC_IP --dport 8008 -j ACCEPT
+sudo iptables -t filter -I FORWARD -p tcp -s $HOST_PUBLIC_IP --dport 8008 -j ACCEPT
 
 # System configuration
 sudo cp snippets/nginx.conf /etc/nginx/conf.d/dyniptables.conf
