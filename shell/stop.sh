aac93f2da7f708fa1ca89aa0a8db83fc5df9fad1
Orzo Cogorzo <orzocogorzo@hotmail.com>
2022-11-15 18:35:49 +0100

    feat: truncate status file on process stop

--- shell/stop.sh
+++ shell/stop.sh
@@ -5,6 +5,7 @@ cd $(dirname $0) && cd ..
 if [ -f var/process.pid ]; then
     kill -9 $(cat var/process.pid)
     rm var/process.pid
+    truncate -s 0 var/status
 else
     echo "No running process"
 fi
