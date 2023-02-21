cb5abd8b5e40fe268a2aa064e3c294794f83e003
Orzo Cogorzo <orzocogorzo@hotmail.com>
2023-02-21 13:36:24 +0100

    feat: update install script

--- sh/install.sh
+++ sh/install.sh
@@ -3,38 +3,63 @@
 DIR=/opt/dynipt-server
 
 # System requirements
-sudo apt update
-sudo apt install -y sudo iptables python3 python3-venv git nginx curl
-
-# User creation
-sudo useradd -M -s /usr/sbin/nologin dynipt
-# read -p "Set dynipt user password: " DYNIPT_PWD
-DYNIPT_PWD=$(date +%s | sha256sum | base64 | head -c 32; echo)
-echo -e "$DYNIPT_PWD\n$DYNIPT_PWD" | sudo passwd dynipt
-sudo usermod -aG sudo dynipt
+echo "Install system requirements..."
+sudo apt -qq update > /dev/null 2>&1
+sudo apt install -qqy sudo iptables python3 python3-venv git nginx curl  > /dev/null 2>&1
+echo "System requirements installeds"
+echo
 
 # Package download
-sudo git clone https://github.com/orzocogorzo/dynipt-server.git $DIR
-sudo chown -R dynipt: $DIR
+echo "Get source code..."
+sudo git clone -q https://github.com/orzocogorzo/dynipt-server.git $DIR
+echo "Source code placed on $DIR"
+echo
+
+# User creation
+echo "Create system user 'dynipt'..."
+sudo useradd -d $DIR -s /usr/sbin/nologin dynipt > /dev/null
+sudo usermod -aG sudo dynipt > /dev/null
+sudo chown -R dynipt: $DIR > /dev/null
+DYNIPT_PWD=$(date +%s | sha256sum | base64 | head -c 32; echo)
+echo -e "$DYNIPT_PWD\n$DYNIPT_PWD" | sudo passwd dynipt > /dev/null
+echo "'dynipt' user created"
+echo
 
 # Setup config
+echo "Configuring dynpt-server service"
 HOST_PUBLIC_IP=$(curl ip.yunohost.org)
-sudo -u dynipt sed -i "s/DYNIPT_HOST_IP=.*/DYNIPT_HOST_IP=$HOST_PUBLIC_IP/" $DIR/.env
-sudo -u dynipt sed -i "s/DYNIPT_PWD=.*/DYNIPT_PWD=$DYNIPT_PWD/" $DIR/.env
-
-# Python requirements
-cd $DIR
-sudo -u dynipt python3 -m venv .venv
-sudo -u dynipt .venv/bin/python -m pip install -r requirements.txt
+sudo -u dynipt sed -i "s/DYNIPT_HOST_IP=.*/DYNIPT_HOST_IP=$HOST_PUBLIC_IP/" $DIR/.env > /dev/null
+sudo -u dynipt sed -i "s/DYNIPT_PWD=.*/DYNIPT_PWD=$DYNIPT_PWD/" $DIR/.env > /dev/null
+sudo chmod 600 $DIR/.env > /dev/null
+echo "Configuration done"
+echo
 
 # Open dynipt server port
-sudo iptables -t filter -I FORWARD -p tcp -s $HOST_PUBLIC_IP --dport 8000 -j ACCEPT
+echo "Opening ports on iptables..."
+sudo iptables -t filter -I FORWARD -p tcp -s $HOST_PUBLIC_IP --dport 8000 -j ACCEPT > /dev/null
+echo "Ports openeds"
+echo
 
-# System configuration
+echo "Configuring dynipt-client systemd service..."
+sudo cp $DIR/snippets/systemd.service /etc/systemd/system/dynipt-server.service
+sudo systemctl daemon-reload
+echo "SystemD service created"
+echo
+
+# Python requirements
+echo "Install python dependencies..."
+cd $DIR
+sudo -u dynipt python3 -m venv .venv
+sudo -u dynipt .venv/bin/python -m pip install -r requirements.txt > /dev/null
+echo "Python is ready"
+echo
+
+# Nginx configuration
+echo "Configuring nginx..."
 sudo cp snippets/nginx.conf /etc/nginx/conf.d/dynipt-server.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo cp snippets/systemd.service /etc/systemd/system/dynipt-server.service
-sudo systemctl daemon-reload
+echo "Nginx is ready"
+echo
 
 # Service start
 sudo nginx -s reload
@@ -43,4 +68,5 @@ sudo systemctl start dynipt-server
 
 echo "DynIPt server is installed and running!"
 echo
+
 .venv/bin/python app.py token
